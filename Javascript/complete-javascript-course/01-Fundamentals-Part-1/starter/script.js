c24f2be1a2c35e34bc582c68ae9d240b67c842b7
Suhagshetty <suhagshetty07@gmail.com>
2026-08-31 09:16:55 +0530

    taking decisons in javascript

--- Javascript/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ Javascript/complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -142,3 +142,28 @@ const currentYear = 2026;
 const friends = ["Srujan", "Shashi", "Rachitha", "Vijay"];
 const infoSuhag = `I am ${fName} a ${job} and the current year is ${currentYear} and my friends are ${friends}`;
 console.log(infoSuhag);
+
+// Taking Decisions:- IF ELSE Statements
+
+const age = 1;
+const DrinkkAge = 18;
+if (age < DrinkAge) {
+  console.log("You cannot drink");
+  console.log(`You have ${DrinkkAge - age} years left`);
+} else {
+  console.log("You can drink");
+}
+
+////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
